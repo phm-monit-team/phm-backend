c2aee10c2a7b39eb07e011eea9ac7d5bf8cc2ee4
mengxiangji <mengxiangji@bytedance.com>
2020-04-28 21:10:22 +0800

    debug log

--- src/app.js
+++ src/app.js
@@ -3,12 +3,13 @@ const Koa = require("koa")
 const bodyParser = require("koa-bodyparser")
 const app = new Koa()
 const router = require("./router")
+const debug = require("debug")
 
 app.use(async (ctx, next) => {
   const start = new Date()
   await next()
   const ms = new Date() - start
-  console.log(`${ctx.method} ${ctx.status} ${ctx.url} - ${ms}ms`)
+  debug("log")(`${ctx.method} ${ctx.status} ${ctx.url} - ${ms}ms`)
 })
 
 app.use(bodyParser())
